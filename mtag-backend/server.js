43ae869e49757b8267cc449498bb1a376dcd7e14
MuhammadHaseeb <haseebdar3333@gmail.com>
2026-03-11 00:54:42 +0500

    Add Cors

--- mtag-backend/server.js
+++ mtag-backend/server.js
@@ -5,7 +5,21 @@ const sqlite3 = require("sqlite3").verbose();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://119.159.147.162:3000',
+    'http://192.168.0.0/24',  // Aapke local network ka IP range
+    'http://127.0.0.1:3000'
+  ],
+  credentials: true
+}));
+app.options('*', cors());
 app.use(express.json());
 
 const db = new sqlite3.Database("./database.sqlite");
